1050b2eab3de053db9905d1b998c923e618159e4
Michael Falk <falk@humatects.de>
2019-11-14 12:24:13 +0100

    "initial project"

---
+++ src/NodeMediaServer.js
@@ -0,0 +1,52 @@
+// NodeJS Media Server
+// https://www.npmjs.com/package/node-media-server
+//
+// Use OBS or ffmpeg to publish the live stream
+// ffmpeg example: ffmpeg -f dshow -framerate 30 -video_size 800x600 -rtbufsize 702000k -i video="MiraBox Video Capture" -vcodec libx264 -preset ultrafast -tune zerolatency -b:v 600k -f flv "rtmp://localhost/live/cocomo"
+//
+// Accessing the live stream
+// RTMP - rtmp://localhost/live/STREAM_NAME
+// http-flv - http://localhost:8000/live/STREAM_NAME.flv
+// websocket-flv - ws://localhost:8000/live/STREAM_NAME.flv
+// HLS - http://localhost:8000/live/STREAM_NAME/index.m3u8
+// DASH - http://localhost:8000/live/STREAM_NAME/index.mpd
+//
+// Use https://www.hlstester.com/ as a streaming client for testing purpose.
+
+const NodeMediaServer = require('node-media-server');
+
+const config = {
+    rtmp: {
+        port: 1935,
+        chunk_size: 60000,
+        gop_cache: true,
+        ping: 30,
+        ping_timeout: 60
+    },
+    http: {
+        port: 8000,
+        mediaroot: './media',
+        allow_origin: '*'
+    },
+    trans: {
+        ffmpeg: 'C:\\Program Files\\ffmpeg-4.0\\bin\\ffmpeg.exe',
+        tasks: [
+            {
+                app: 'live',
+                vc: "copy",
+                vcParam: [],
+                ac: "aac",
+                acParam: ['-ab', '64k', '-ac', '1', '-ar', '44100'],
+                //rtmp:true,
+                //rtmpApp:'live2',
+                hls: true,
+                hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+                dash: true,
+                dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+            }
+        ]
+    }
+};
+
+var mediaserver = new NodeMediaServer(config);
+mediaserver.run();
